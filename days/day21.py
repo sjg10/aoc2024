1dcd56b36c474ca1d3700641aec707a8c3d9d876
Sam Gonshaw <samuel@bios.health>
2024-12-24 08:50:48 +0000

    day21 p1

--- days/day21.py
+++ days/day21.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from heapq import heappop, heappush
-
+from itertools import permutations, product
+from functools import cache
 
 @dataclass(frozen=True, order=True)
 class Pos:
@@ -12,10 +12,6 @@ class Pos:
 
     def __sub__(self, o):
         return Pos(self.r - o.r, self.c - o.c)
-
-    def dist(self, o):
-        return abs(self.r - o.r) + abs(self.c - o.c)
-
 
 class NumPads:
     NUMLOCS = {
@@ -38,12 +34,11 @@ class NumPads:
         "v": Pos(1, 1),
         ">": Pos(1, 2),
     }
-    DLCI = {  # DLOCS inverted
-        Pos(0, 1): "^",
-        Pos(0, 2): "A",
-        Pos(1, 0): "<",
-        Pos(1, 1): "v",
-        Pos(1, 2): ">",
+    PTODIR = {
+        "^": Pos(-1, 0),
+        "<": Pos(0, -1),
+        "v": Pos(1, 0),
+        ">": Pos(0, 1),
     }
 
     def __init__(self, inlines):
@@ -51,141 +46,80 @@ class NumPads:
         for l in inlines:
             self.targets.append(l.strip())
 
-    @staticmethod
-    def simulate(robots, target, depth=2):
-        """
-        Given robot positions, a final target, and a depth at which A is pressed:
-        Simulate pressing A and if this gets us not further from target, return True, else False
-        """
-        push = False
-        if depth > 1:  # A is pressed by a robot controlling a direction robot
-            match NumPads.DLCI[robots[depth]]:
-                case "^":
-                    if robots[depth - 1].r != 0 and robots[depth - 1].c != 0:
-                        robots[depth - 1] = Pos(
-                            robots[depth - 1].r - 1, robots[depth - 1].c
-                        )
-                        push = True
-                case "v":
-                    if robots[depth - 1].r != 1:
-                        robots[depth - 1] = Pos(
-                            robots[depth - 1].r + 1, robots[depth - 1].c
-                        )
-                        push = True
-                case ">":
-                    if robots[depth - 1].c != 2:
-                        robots[depth - 1] = Pos(
-                            robots[depth - 1].r, robots[depth - 1].c + 1
-                        )
-                        push = True
-                case "<":
-                    if robots[depth - 1].c != 0 and (
-                        robots[depth - 1].r != 0 or robots[depth - 1].c != 1
-                    ):
-                        robots[depth - 1] = Pos(
-                            robots[depth - 1].r, robots[depth - 1].c - 1
-                        )
-                        push = True
-                case "A":
-                    push = NumPads.simulate(robots, target, depth - 1)
-        else:  # A is pressed by a robot controlling a digit robot
-            match NumPads.DLCI[robots[depth]]:
-                case "^":
-                    if robots[0].r != 0:
-                        nr0 = Pos(robots[0].r - 1, robots[0].c)
-                        if target.dist(nr0) < target.dist(robots[0]):
-                            robots[0] = nr0
-                            push = True
-                case "v":
-                    if robots[0].r != 3 and (robots[0].r != 2 or robots[0].c != 0):
-                        nr0 = Pos(robots[0].r + 1, robots[0].c)
-                        if target.dist(nr0) < target.dist(robots[0]):
-                            robots[0] = nr0
-                            push = True
-                case ">":
-                    if robots[depth - 1].c != 2:
-                        nr0 = Pos(robots[0].r, robots[0].c + 1)
-                        if target.dist(nr0) < target.dist(robots[0]):
-                            robots[0] = nr0
-                            push = True
-                case "<":
-                    if robots[0].c != 0 and (robots[0].c != 1 or robots[0].r != 3):
-                        nr0 = Pos(robots[0].r, robots[0].c - 1)
-                        if target.dist(nr0) < target.dist(robots[0]):
-                            robots[0] = nr0
-                            push = True
-                case "A":  # Digit robot told to push - only allow correct target
-                    if robots[0] == target:
-                        push = True
-        return push
+    def dirtostr(d):
+        s = []
+        if d.r < 0:
+            s.append("^" * (-d.r))
+        if d.r > 0:
+            s.append("v" * (d.r))
+        if d.c < 0:
+            s.append("<" * (-d.c))
+        if d.c > 0:
+            s.append(">" * (d.c))
+        return set("".join(x) for x in permutations(s))
 
-    @staticmethod
-    def move(cur, target):
-        """
-        Find the shortest input sequence to move from cur to target on the digit pad and press it
-        """
-        visited = set()
-        # strlen, str, robot1 pos, robot2 pos, robot3 pos
-        heap = [(0, "", cur, Pos(0, 2), Pos(0, 2))]
-        while len(heap) > 0:
-            d, u, r1, r2, r3 = heappop(heap)
-            # dont go the opposite direction to last char - waste of a try
-            last = u[-1] if len(u) else None
+    def badmove(cur, path, numpad):
+        for p in path:
+            cur = cur + NumPads.PTODIR[p]
+            if numpad and cur == Pos(3, 0):
+                return True
+            if not numpad and cur == Pos(0, 0):
+                return True
+        return False
 
-            if r1 == target:
-                return u
+    @cache
+    def gennext(w): # TODO: cache better?!?
+        cur = NumPads.DLOCS["A"]
+        nws = []
+        for p in w:
+            nxt = NumPads.DLOCS[p]
+            newways = [
+                x + "A"
+                for x in filter(
+                    lambda w: not NumPads.badmove(cur, w, False),
+                    NumPads.dirtostr(nxt - cur),
+                )
+            ]
+            cur = nxt
+            if len(nws) == 0:
+                nws = newways
+            else:
+                nws = ["".join(x) for x in product(nws, newways)]
+        return nws
 
-            # Try each direction
+    def move(cur, target, midrobots):
+        ways = [
+            x + "A"
+            for x in filter(
+                lambda w: not NumPads.badmove(cur, w, True),
+                NumPads.dirtostr(target - cur),
+            )
+        ]
 
-            nxt = u + "^"
-            if last != "v" and nxt not in visited and r3.r != 0 and r3.c != 0:
-                visited.add(nxt)
-                heappush(heap, (d + 1, nxt, r1, r2, Pos(r3.r - 1, r3.c)))
+        for i in range(midrobots):
+            nwys = []
+            for w in ways:
+                nwys.extend(NumPads.gennext(w))
+            ways = nwys
+        return ways
 
-            nxt = u + "v"
-            if last != "^" and nxt not in visited and r3.r != 1:
-                visited.add(nxt)
-                heappush(heap, (d + 1, nxt, r1, r2, Pos(r3.r + 1, r3.c)))
-
-            nxt = u + ">"
-            if last != "<" and nxt not in visited and r3.c != 2:
-                visited.add(nxt)
-                heappush(heap, (d + 1, nxt, r1, r2, Pos(r3.r, r3.c + 1)))
-
-            nxt = u + "<"
-            if (
-                last != ">"
-                and nxt not in visited
-                and r3.c != 0
-                and (r3.r != 0 or r3.c != 1)
-            ):
-                visited.add(nxt)
-                heappush(heap, (d + 1, nxt, r1, r2, Pos(r3.r, r3.c - 1)))
-
-            nxt = u + "A"
-            if nxt not in visited:
-                robots = [r1, r2, r3]
-                push = NumPads.simulate(robots, target)
-                if push:
-                    visited.add(nxt)
-                    heappush(heap, (d + 1, nxt, *robots))
-
-    def p1(self):
-        p1 = 0
+    def run(self, midrobots):
+        ret = 0
         for t in self.targets:
             c = ""
             lastdigit = "A"
             for digit in t:
                 target = self.NUMLOCS[digit]
-                mv = NumPads.move(self.NUMLOCS[lastdigit], target)
+                mvs = NumPads.move(self.NUMLOCS[lastdigit], target, midrobots)
+                mv = min(mvs, key=len)
                 print(f"From {lastdigit} to {digit} = {mv}")
                 c += mv
                 lastdigit = digit
             print("DONE", t, c, len(c))
-            p1 += len(c) * int(t[:-1])
-        return p1
+            ret += len(c) * int(t[:-1])
+        return ret
 
 
 def run(inlines):
     np = NumPads(inlines)
-    return np.p1(), None
+    return np.run(2), np.run(25)
